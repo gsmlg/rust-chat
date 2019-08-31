5f92fc8f911fd30653e47be11f66c1c39d353188
gsmlg <me@gsmlg.org>
2019-08-31 12:42:46 +0800

    update loop

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 extern crate term;
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 fn main () {
-    let mut t = term::stdout().unwrap();
-    t.fg(term::color::GREEN).unwrap();
-    write!(t, "hello, ").unwrap();
+    let mut rl = Editor::<()>::new();
 
-    t.fg(term::color::RED).unwrap();
-    writeln!(t, "world!").unwrap();
+    loop {
+        let name = "Josh";
+        let mut who = String::from(name);
+        who.push_str(": ");
+        let readline = rl.readline(who.as_str());
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                let mut t = term::stdout().unwrap();
+                t.fg(term::color::RED).unwrap();
+                write!(t, "{}: ", name).unwrap();
+                t.fg(term::color::GREEN).unwrap();
+                write!(t, "{}", line).unwrap();
 
-    t.reset().unwrap();
+                t.reset().unwrap();
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
 }
